843af79e1d0748d592fdb9b49c18192692d7c4f0
Ankushh0027 <ankush270008@gmail.com>
2026-08-02 19:40:02 +0530

    feat: add dataset inventory engine

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,23 @@
-""
+"""
+Dravya AI Data Module
+"""
+
+from src.data.paths import (
+    EXTERNAL_DATASET_ROOT,
+    DATASET_PATHS,
+    SUPPORTED_IMAGE_EXTENSIONS,
+    ARCHIVE_EXTENSIONS,
+    METADATA_EXTENSIONS,
+)
+from src.data.inventory import InventoryScanner
+from src.data.manifest import ManifestGenerator
+
+__all__ = [
+    "EXTERNAL_DATASET_ROOT",
+    "DATASET_PATHS",
+    "SUPPORTED_IMAGE_EXTENSIONS",
+    "ARCHIVE_EXTENSIONS",
+    "METADATA_EXTENSIONS",
+    "InventoryScanner",
+    "ManifestGenerator",
+]
