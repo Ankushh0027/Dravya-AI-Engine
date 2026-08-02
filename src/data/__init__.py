75568e6a464c95b23bc9e46e6c12d040b2f910e5
Ankushh0027 <ankush270008@gmail.com>
2026-08-02 20:07:25 +0530

    feat: add canonical taxonomy mapping and review layer

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -13,6 +13,14 @@ from src.data.inventory import InventoryScanner
 from src.data.manifest import ManifestGenerator
 from src.data.deduplication import ExactDuplicateDetector, compute_file_sha256
 from src.data.harmonization import ClassHarmonizationAnalyzer, parse_class_name
+from src.data.taxonomy import (
+    CanonicalPlant,
+    TaxonomyMapping,
+    MappingStatus,
+    generate_canonical_plant_id,
+)
+from src.data.taxonomy_validator import TaxonomyValidator
+from src.data.taxonomy_manager import TaxonomyManager
 
 __all__ = [
     "EXTERNAL_DATASET_ROOT",
@@ -26,6 +34,13 @@ __all__ = [
     "compute_file_sha256",
     "ClassHarmonizationAnalyzer",
     "parse_class_name",
+    "CanonicalPlant",
+    "TaxonomyMapping",
+    "MappingStatus",
+    "generate_canonical_plant_id",
+    "TaxonomyValidator",
+    "TaxonomyManager",
 ]
+
 
 
